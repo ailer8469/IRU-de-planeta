bf445b9ae64f023cc724b987d51a686650ec15ff
ruru1225 <ailer8469@email.com>
2024-09-14 18:35:06 +0800

    9/14

--- dev/js/layout/cursor.js
+++ dev/js/layout/cursor.js
@@ -1,23 +1,23 @@
 $(document).ready(function() {
-    $('body').append('<div class="cursor"></div>');
-  
-    $(document).on('mousemove', function(e) {
-      $('.cursor').css({
-        top: e.clientY + 'px',
-        left: e.clientX + 'px'
-      });
-  
-      var $trail = $('<div class="trail"></div>').appendTo('body');
-      $trail.css({
-        top: e.clientY + 'px',
-        left: e.clientX + 'px'
-      });
-  
-      setTimeout(function() {
-        $trail.css('opacity', '0');
-        setTimeout(function() {
-          $trail.remove();
-        }, 500);
-      }, 0);
+  $('body').append('<div class="cursor"></div>');
+
+  $(document).on('mousemove', function(e) {
+    $('.cursor').css({
+      top: e.pageY + 'px',
+      left: e.pageX + 'px'
     });
+
+    var $trail = $('<div class="trail"></div>').appendTo('body');
+    $trail.css({
+      top: e.pageY + 'px',
+      left: e.pageX + 'px'
+    });
+
+    setTimeout(function() {
+      $trail.css('opacity', '0');
+      setTimeout(function() {
+        $trail.remove();
+      }, 500);
+    }, 0);
   });
+});
