543035860d4bd72d2b5b677afe307e35be675507
ruru1225 <ailer8469@email.com>
2024-09-16 16:08:04 +0800

    9/16

--- dev/js/layout/model.js
+++ dev/js/layout/model.js
@@ -5,20 +5,20 @@ $(document).ready(function() {
 
     // 按鈕點擊事件，顯示模態框
     $click.on('click', function() {
-        $model.toggleClass("animate");
+        $model.toggleClass("animate").css('pointerEvents','auto');
         $('body').css('overflow', 'hidden'); 
     });
 
     // 關閉按鈕點擊事件，隱藏模態框
     $exit.on('click', function() {
-        $model.toggleClass("animate");
+        $model.toggleClass("animate").css('pointerEvents','none');
         $('body').css('overflow', ''); 
     });
 
     // 當點擊窗口以外的區域時，隱藏模態框
     $(window).on('click', function(event) {
         if ($(event.target).is($model)) {
-            $model.toggleClass("animate");
+            $model.toggleClass("animate").css('pointerEvents','none');
             $('body').css('overflow', ''); 
         }
     });
