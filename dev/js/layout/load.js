9609c994ee838b91c93ed171df08f7effbd16960
ruru1225 <ailer8469@email.com>
2024-09-26 22:47:50 +0800

    0926

--- dev/js/layout/load.js
+++ dev/js/layout/load.js
@@ -1,24 +1,38 @@
- // loading
-$(function(){
-    const totalLoads = $('.load').length;
+$(function() {
     const fadeInDuration = 100;
-    const waitDuration = 300;
+    let displayedImages = 0;
+
+    function showImage(index) {
+        $('.load').css('opacity', 0);
+        const currentLoad = $('.load').eq(index);
+        currentLoad.css('opacity',1);
+    }
+
     $('body').waitForImages({
-        waitForAll: true, 
+        waitForAll: true,
         each: function(loaded, count, success) {
             var pNum = parseInt((loaded / count) * 100);
-            $('#loadpage').find('#progress').text(pNum+'%');
-
-            // $('.load').each(function(index) {
-            //     const currentLoad = $(this);
-            //     currentLoad.delay(index * (fadeInDuration + waitDuration)).fadeTo(fadeInDuration, 1).delay(waitDuration).fadeTo(fadeInDuration, 0);
-            // });
-        }, 
-        finished: function() {
-            // 等待所有元素完成後隱藏 loading 頁面
-            setTimeout(function() {
-                $('#loading').fadeOut(500);
-            }, totalLoads * (fadeInDuration + waitDuration)); // 計算總時間
+            // 判斷當前進度並顯示圖片
+            if (pNum >= 20 && displayedImages === 0) {
+                showImage(0);
+                displayedImages++;
+            } else if (pNum >= 40 && displayedImages === 1) {
+                showImage(1);
+                displayedImages++;
+            } else if (pNum >= 60 && displayedImages === 2) {
+                showImage(2);
+                displayedImages++;
+            } else if (pNum >= 80 && displayedImages === 3) {
+                showImage(3);
+                displayedImages++;
+            } else if (pNum >= 100 && displayedImages === 4) {
+                showImage(4);
+                displayedImages++;
             }
-        });
+        },
+        finished: function() {
+            // 確保最後一張圖片顯示
+            $('#loading').fadeOut(500); 
+        },
+    });
 });
