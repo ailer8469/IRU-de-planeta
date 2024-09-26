80304be3e6ef71d310ea8f88e6eb03953b22c0e0
ruru1225 <ailer8469@email.com>
2024-09-26 22:22:05 +0800

    0926

--- dev/js/layout/load.js
+++ dev/js/layout/load.js
@@ -1,17 +1,20 @@
  // loading
- $(function(){
+$(function(){
     const totalLoads = $('.load').length;
     const fadeInDuration = 100;
     const waitDuration = 300;
     $('body').waitForImages({
         waitForAll: true, 
         each: function(loaded, count, success) {
+            var pNum = parseInt((loaded / count) * 100);
+            $('#loadpage').find('#progress').text(pNum+'%');
+
+            // $('.load').each(function(index) {
+            //     const currentLoad = $(this);
+            //     currentLoad.delay(index * (fadeInDuration + waitDuration)).fadeTo(fadeInDuration, 1).delay(waitDuration).fadeTo(fadeInDuration, 0);
+            // });
         }, 
         finished: function() {
-            $('.load').each(function(index) {
-                const currentLoad = $(this);
-                currentLoad.delay(index * (fadeInDuration + waitDuration)).fadeTo(fadeInDuration, 1).delay(waitDuration).fadeTo(fadeInDuration, 0);
-            });
             // 等待所有元素完成後隱藏 loading 頁面
             setTimeout(function() {
                 $('#loading').fadeOut(500);
